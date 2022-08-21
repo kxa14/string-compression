cb8af737dd1ee2655ec9a65d01cde54babde15b2
kaixin <kaixinang94@gmail.com>
2022-08-21 11:45:38 +0800

    WIP: non-zero exit code

--- src/main/scala/com/educationFirst/Main.scala
+++ src/main/scala/com/educationFirst/Main.scala
@@ -2,9 +2,22 @@ package com.educationFirst
 
 object Main extends App {
   {
-    if (s"${args.mkString("").stripMargin}".isEmpty) {
+    if (
+      s"${args.mkString("").stripMargin}".nonEmpty && s"${args.mkString("").stripMargin}"
+        .matches("""[a-z][1-9]+""")
+    ) {
+      new Parser()
+        .compress(s"${args.mkString("").stripMargin}")
+      System.exit(0)
+    } else if (s"${args.mkString("").stripMargin}".isEmpty) {
       println("Missing an input. Please try again.")
-    } else
-      println(new Parser().compress(s"${args.mkString("").stripMargin}"))
+      System.exit(1)
+    } else {
+      {
+        println(s"${args.mkString("")} is an invalid input")
+        System.exit(128)
+
+      }
+    }
   }
 }
