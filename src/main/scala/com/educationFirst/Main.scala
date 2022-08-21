19576b496a2cf437e9449e22a8c58691e1ef25d0
kaixin <kaixinang94@gmail.com>
2022-08-22 07:57:54 +0800

    WIP

--- src/main/scala/com/educationFirst/Main.scala
+++ src/main/scala/com/educationFirst/Main.scala
@@ -3,8 +3,8 @@ package com.educationFirst
 object Main extends App {
   {
     if (
-      s"${args.mkString("").stripMargin}".nonEmpty && s"${args.mkString("").stripMargin}"
-        .matches("""[a-z][1-9]+""")
+      s"${args.mkString("").stripMargin}".nonEmpty && """[a-z][1-9]+""".r
+        .matches(s"${args.mkString("").stripMargin}")
     ) {
       new Parser()
         .compress(s"${args.mkString("").stripMargin}")
